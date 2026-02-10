028df4846484487bab141b9c096992a4ac2a960f
Marcos Jimgar <marcos.jimenez.garcia.alu@iesfernanadoaguilar.es>
2026-02-10 13:58:46 +0100

    kikk

--- src/test/java/testGetInfoTest.java
+++ src/test/java/testGetInfoTest.java
@@ -1,6 +1,9 @@
-package com.example.demo.Controller;
 
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,21 +13,24 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.example.demo.proyectoAddons.controller.AddonController;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
 @WebMvcTest(AddonController.class)
 @AutoConfigureMockMvc(addFilters = false)
-
 class GetInfoTest {
+
     @Autowired
     private MockMvc mockMvc;
-    //GetInfoTest1();
-    
+
+    //TEST DE INTEGRACIÓN
+
     @Test
-    void GetInfoTest1() throws Exception {
+    @DisplayName("Debe responder 200 OK")
+    void getInfo_ok() throws Exception {
         mockMvc.perform(get("/api/addon"))
-               .andExpect(status().isOk());      
+               .andExpect(status().isOk());
     }
 }
+    
